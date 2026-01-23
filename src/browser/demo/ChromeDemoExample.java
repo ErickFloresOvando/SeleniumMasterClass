6cd470735c2d4e23826c4acfafd0214676e09d64
ErickFloresOvando <erickfloresovando@gmail.com>
2026-01-23 11:14:51 -0600

    Se sube archivo ChromeDemo y ChromeDemoExample

--- src/browser/demo/ChromeDemoExample.java
+++ src/browser/demo/ChromeDemoExample.java
@@ -3,27 +3,19 @@ package browser.demo;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-
-import java.util.Hashtable;
-import java.util.Map;
-
-public class ChromeDemo {
+public class ChromeDemoExample {
     public static void main(String[] args) {
-
         //Open Google Chrome Browser
         System.setProperty("webdriver.chrome.driver", "/Users/erickfloresovando/Downloads/chromedriver-mac-arm64/chromedriver");
-        Map<String, Object> preferences = new Hashtable<String, Object>();
-        preferences.put("download.default_directory", "F:\\");
-
 
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--start-maximized"); //Maximixe the window
+        //options.addArguments("--disable-javascript");
+        //options.addArguments("--disable-notifications");
 
-        options.setExperimentalOption("prefs", preferences);
+        options.addArguments("--incognito"); //Open in incognito mode
 
         ChromeDriver driver = new ChromeDriver(options);
-        driver.get("https://www.google.com");
-
+        driver.get("https://www.justdial.com");
 
 
     }
